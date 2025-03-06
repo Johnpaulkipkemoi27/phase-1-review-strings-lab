c5a4ecbebd91fd27ec6ee145ac502031e077f7a4
John Paul <johntetoo27@gmail.com>
2025-03-06 12:14:25 +0300

    new changes

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'John Paul';
+const welcomeMessage ="Welcome to Flatbook, " +'John Paul!';
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, "+ 'JOHN PAUL' + '!';
+const shortGreeting ="Welcome, "+'J'+'!'
